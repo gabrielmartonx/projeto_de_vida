e5f740b4268beace8fc84b0dd75000f699843384
gabrielmartonx <160757642+gabrielmartonx@users.noreply.github.com>
2024-04-22 10:49:51 -0300

    s

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
@@ -14,6 +15,31 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-10-30T00:00:00");
+const tempoObjetivo3 = new Date("2024-11-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
+
+const tempos =
+[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+
+function atualizaCronometro(){
+     for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo(tempos[i]); } 
+}
+
+
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+
+comecaCronometro();
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -26,33 +52,8 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return [dias,horas,minutos,segundos]
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return [0,0,0,0];
+        return "Prazo Finalizado";
     }
 }
-document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
-document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
-document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
-document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-
-
-
-for (let i=0; i<contadores.length;i++){
-    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
-    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
-    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
-}
-
-
-document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
-document.getElementById("seg1").textContent = calculaTempo(tempos[1])[3];
-document.getElementById("seg2").textContent = calculaTempo(tempos[2])[3];
-document.getElementById("seg3").textContent = calculaTempo(tempos[3])[3];
-
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-
-
-
-
